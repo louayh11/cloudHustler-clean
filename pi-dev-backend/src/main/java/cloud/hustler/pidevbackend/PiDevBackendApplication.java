ce3a80c42aaded407bfe062dd42159fad98557aa
HamZeus95 <benali.hamza@esprit.tn>
2025-02-28 00:47:34 +0100

    init app

---
+++ pi-dev-backend/src/main/java/cloud/hustler/pidevbackend/PiDevBackendApplication.java
@@ -0,0 +1,13 @@
+package cloud.hustler.pidevbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiDevBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiDevBackendApplication.class, args);
+    }
+
+}
---
+++ pi-dev-backend/src/test/java/cloud/hustler/pidevbackend/PiDevBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.hustler.pidevbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiDevBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
