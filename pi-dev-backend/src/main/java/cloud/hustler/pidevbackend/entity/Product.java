7d84845c6050fe16faab70d016d4de2bde4b6b79
Mohamed Dhia Alaya <dhiaalaya3@gmail.com>
2025-04-24 10:09:07 +0100

    add missing files and fix them

---
+++ pi-dev-backend/src/main/java/cloud/hustler/pidevbackend/entity/Product.java
@@ -0,0 +1,40 @@
+package cloud.hustler.pidevbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    UUID uuid_product;
+    String name;
+    String description;
+    double price;
+    int quantity;
+    boolean isAvailable;
+    String imageUrl;
+
+
+    @ManyToOne
+    Order order;
+
+    @ManyToOne
+    Farmer farmer;
+
+    @ManyToOne
+    ProductCategory productCategory;
+
+
+
+}
